f9d6b96b84013fab300323b3dc3d972d7477f3eb
wltsmrz <wlt@smrz.dev>
2020-11-13 08:49:53 +0000

    Fixins

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 
-Porter stemmer interpretation in carp, ossibly inefficient
+Porter stemmer interpretation in carp
 
 Compiled to webassembly via emscripten:
 
 ```
-emcc main.c -O2 -I/home/w/carp/Carp/core -I.. -s ALLOW_MEMORY_GROWTH=1 \
--s EXPORTED_FUNCTIONS='["_malloc","_free","_stem_MINUS_cstr","_cv_MINUS_form_MINUS_cstr"]' \
+emcc main.c -O3 -g0 -I/home/w/carp/Carp/core -I.. -s ALLOW_MEMORY_GROWTH=1 \
+-s EXPORTED_FUNCTIONS='["_malloc","_free","_stem_MINUS_cstr"]' \
 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap","stringToUTF8","lengthBytesUTF8","UTF8ToString"]'
 ```
-
 
--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const Module = require('./a.out.js')
 const stem = Module.cwrap('stem_MINUS_cstr', 'number', [ 'number' ])
-const cvForm = Module.cwrap('cv_MINUS_form_MINUS_cstr', 'number', [ 'number' ])
 
 const api = {
   stem: function(str) {
@@ -13,21 +12,10 @@ const api = {
     Module._free(inPtr)
     return res
   },
-
-  cvForm: function(str) {
-    const byteLen = Module.lengthBytesUTF8(str) +1
-    const inPtr = Module._malloc(byteLen)
-    Module.stringToUTF8(str, inPtr, byteLen)
-    const outPtr = cvForm(inPtr)
-    const res = Module.UTF8ToString(outPtr)
-    Module._free(outPtr)
-    Module._free(inPtr)
-    return res
-  }
 }
 
 Module.onRuntimeInitialized = function() {
-  console.log('cvForm: %s', 'programming', api.cvForm('programming'))
   console.log('stem: %s -> %s', 'programming', api.stem('programming'))
+  console.log('stem: %s -> %s', 'vileness', api.stem('vileness'))
 }
 
