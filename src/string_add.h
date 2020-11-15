b22cf9c1695df5cccdce241d0cf8da6d8cbb88e1
wltsmrz <wlt@smrz.dev>
2020-11-15 21:39:47 +0000

    Cleanup

--- src/string_add.h
+++ src/string_add.h
@@ -1,3 +1,6 @@
+size_t String_utf8len_(const String* p) {
+  return utf8len(*p);
+}
 
 String String_to_lower_(const String* p) {
   String r = String_copy(p);
@@ -5,3 +8,4 @@ String String_to_lower_(const String* p) {
   while (i--) *(r + i) = tolower(*(r + i));
   return r;
 }
+
